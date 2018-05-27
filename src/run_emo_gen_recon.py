5908d20cc58bd233ac717503d4aeeca37ed64603
GangHu <hudou1107@163.com>
2018-05-27 20:37:26 +0800

    a server for emotion and gender recognition.

---
+++ src/run_emo_gen_recon.py
@@ -0,0 +1,17 @@
+'''
+test file for the class of emotion and gender recognition.
+Author: Gang Hu
+Date: 28th, Aug, 2017
+'''
+
+from emo_gen_recon import Emo_gen_recon
+import cv2
+
+img=cv2.imread('t.JPG',cv2.IMREAD_COLOR)
+
+emo_gen_recon = Emo_gen_recon()
+res = emo_gen_recon.det_emo_gen_reco(img)
+res1 = emo_gen_recon.det_emo_gen_reco(img)
+print ('test~~~~~~~~~~~~~~')
+print (res)
+print (res1)
